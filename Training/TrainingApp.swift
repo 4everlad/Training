664a2b7274af42bf3fc8cb2a9b1e16eba953b015
4everlad <d.s.bakulin@gmail.com>
2022-06-28 13:54:25 +0300

    Initial Commit

---
+++ Training/TrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingApp.swift
+//  Training
+//
+//  Created by Dmitry Bakulin on 28.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
